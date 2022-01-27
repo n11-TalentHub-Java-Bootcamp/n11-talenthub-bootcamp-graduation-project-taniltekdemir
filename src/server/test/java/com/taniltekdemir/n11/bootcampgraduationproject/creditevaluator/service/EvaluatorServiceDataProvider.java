b0153868b61d3d4e14e2d666f0132cd4dc759a50
aniltekdemir <anil.tekdemir@kolaysoft.com.tr>
2022-01-27 21:31:08 +0300

    unit test is added for EvaluatorService

--- src/server/test/java/com/taniltekdemir/n11/bootcampgraduationproject/creditevaluator/service/EvaluatorServiceDataProvider.java
+++ src/server/test/java/com/taniltekdemir/n11/bootcampgraduationproject/creditevaluator/service/EvaluatorServiceDataProvider.java
@@ -1,6 +1,7 @@
 package com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.service;
 
 import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.dto.EvaluationDto;
+import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.entity.EvaluateReport;
 import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.enums.EnumEvaluateStatus;
 import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.strategy.substrategy.payload.EvaluationResult;
 
@@ -11,15 +12,189 @@ public class EvaluatorServiceDataProvider {
     public static EvaluationDto getEvaluationDtoForStrategy3WithGuarantee() {
         EvaluationDto evaluationDto = new EvaluationDto();
         evaluationDto.setScore(1000);
-        evaluationDto.setSalary(BigDecimal.valueOf(15000));
-        evaluationDto.setGuarantee(BigDecimal.valueOf(50000));
+        evaluationDto.setSalary(BigDecimal.valueOf(15));
+        evaluationDto.setGuarantee(BigDecimal.valueOf(50));
         return evaluationDto;
     }
 
     public static EvaluationResult getEvaluationResultForStrategy3WithGuarantee() {
         EvaluationResult evaluationResult = new EvaluationResult();
         evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
-        evaluationResult.setLimit(BigDecimal.valueOf(85000));
+        evaluationResult.setLimit(BigDecimal.valueOf(85));
         return evaluationResult;
     }
+
+    public static EvaluationDto getEvaluationDtoForStrategy3WithGuaranteeless() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(1000);
+        evaluationDto.setSalary(BigDecimal.valueOf(15));
+        evaluationDto.setGuarantee(null);
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForStrategy3WithGuaranteeless() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(60));
+        return evaluationResult;
+    }
+
+    public static EvaluationDto getEvaluationDtoForStrategy1WithGuarantee() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(499);
+        evaluationDto.setSalary(BigDecimal.valueOf(15));
+        evaluationDto.setGuarantee(BigDecimal.valueOf(50));
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForStrategy1WithGuarantee() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.REJECTED);
+        evaluationResult.setLimit(BigDecimal.ZERO);
+        return evaluationResult;
+    }
+
+    public static EvaluationDto getEvaluationDtoForStrategy1WithGuaranteeless() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(499);
+        evaluationDto.setSalary(BigDecimal.valueOf(15));
+        evaluationDto.setGuarantee(null);
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForStrategy1WithGuaranteeless() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.REJECTED);
+        evaluationResult.setLimit(BigDecimal.ZERO);
+        return evaluationResult;
+    }
+
+// score 500 testti Guarantee BigDecimal.ZERO
+    public static EvaluationDto getEvaluationDtoForStrategy2WithGuaranteeless() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(500);
+        evaluationDto.setSalary(BigDecimal.valueOf(50));
+        evaluationDto.setGuarantee(BigDecimal.ZERO);
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForStrategy2WithGuaranteeless() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(10000));
+        return evaluationResult;
+    }
+
+    // score 500 testti Guarantee null
+    public static EvaluationDto getEvaluationDtoForStrategy2WithGuaranteeIsNull() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(500);
+        evaluationDto.setSalary(BigDecimal.valueOf(50));
+        evaluationDto.setGuarantee(null);
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForStrategy2WithGuaranteeIsNull() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(10000));
+        return evaluationResult;
+    }
+
+    public static EvaluationDto getEvaluationDtoForSubStrategy1WithGuaranteeIsNull() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(800);
+        evaluationDto.setSalary(BigDecimal.valueOf(50));
+        evaluationDto.setGuarantee(null);
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForSubStrategy1WithGuaranteeIsNull() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(10000));
+        return evaluationResult;
+    }
+
+    public static EvaluationDto getEvaluationDtoForSubStrategy1WithGuarantee() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(800);
+        evaluationDto.setSalary(BigDecimal.valueOf(50));
+        evaluationDto.setGuarantee(BigDecimal.valueOf(100));
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForSubStrategy1WithGuarantee() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(10010));
+        return evaluationResult;
+    }
+
+    public static EvaluationDto getEvaluationDtoForSubStrategy2WithGuaranteeIsNull() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(800);
+        evaluationDto.setSalary(BigDecimal.valueOf(5000));
+        evaluationDto.setGuarantee(null);
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForSubStrategy2WithGuaranteeIsNull() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(20000));
+        return evaluationResult;
+    }
+
+    public static EvaluationDto getEvaluationDtoForSubStrategy2WithGuarantee() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(800);
+        evaluationDto.setSalary(BigDecimal.valueOf(5000));
+        evaluationDto.setGuarantee(BigDecimal.valueOf(100));
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForSubStrategy2WithGuarantee() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(20020));
+        return evaluationResult;
+    }
+
+    public static EvaluationDto getEvaluationDtoForSubStrategy3WithGuaranteeIsNull() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(800);
+        evaluationDto.setSalary(BigDecimal.valueOf(15000));
+        evaluationDto.setGuarantee(null);
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForSubStrategy3WithGuaranteeIsNull() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(30000));
+        return evaluationResult;
+    }
+
+    public static EvaluationDto getEvaluationDtoForSubStrategy3WithGuarantee() {
+        EvaluationDto evaluationDto = new EvaluationDto();
+        evaluationDto.setScore(800);
+        evaluationDto.setSalary(BigDecimal.valueOf(15000));
+        evaluationDto.setGuarantee(BigDecimal.valueOf(100));
+        return evaluationDto;
+    }
+
+    public static EvaluationResult getEvaluationResultForSubStrategy3WithGuarantee() {
+        EvaluationResult evaluationResult = new EvaluationResult();
+        evaluationResult.setEvaluateStatus(EnumEvaluateStatus.ACCEPTED);
+        evaluationResult.setLimit(BigDecimal.valueOf(30025));
+        return evaluationResult;
+    }
+
+    public static EvaluateReport getEvaluateReport(){
+        EvaluateReport evaluateReport = new EvaluateReport();
+        evaluateReport.setId(1L);
+        return evaluateReport;
+    }
+
 }
--- src/server/test/java/com/taniltekdemir/n11/bootcampgraduationproject/creditevaluator/service/EvaluatorServiceTest.java
+++ src/server/test/java/com/taniltekdemir/n11/bootcampgraduationproject/creditevaluator/service/EvaluatorServiceTest.java
@@ -1,6 +1,7 @@
 package com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.service;
 
 import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.dto.EvaluationDto;
+import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.entity.EvaluateReport;
 import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.repository.EvaluatorRepository;
 import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.service.entityService.EvaluatorEntityService;
 import com.taniltekdemir.n11.bootcampgraduationproject.creditevaluator.strategy.substrategy.payload.EvaluationResult;
@@ -8,7 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class EvaluatorServiceTest {
@@ -21,12 +24,125 @@ class EvaluatorServiceTest {
     private EvaluatorRepository evaluatorRepository;
 
     @Test
-    void calculateCredit() {
+    void calculateCreditForStrategy3WithGurantee() {
         EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForStrategy3WithGuarantee();
         EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForStrategy3WithGuarantee();
 
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
 
+        assertEquals(evaluationResult, result);
     }
+
+    @Test
+    void calculateCreditForStrategy3WithGuaranteeless() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForStrategy3WithGuaranteeless();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForStrategy3WithGuaranteeless();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForStrategy1WithGuarantee() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForStrategy1WithGuarantee();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForStrategy1WithGuarantee();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForStrategy1WithGuaranteeless() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForStrategy1WithGuaranteeless();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForStrategy1WithGuaranteeless();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForStrategy2WithGuaranteeless() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForStrategy2WithGuaranteeless();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForStrategy2WithGuaranteeless();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForStrategy2WithGuaranteeIsNull() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForStrategy2WithGuaranteeIsNull();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForStrategy2WithGuaranteeIsNull();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForSubStrategy1WithGuaranteeIsNull() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForSubStrategy1WithGuaranteeIsNull();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForSubStrategy1WithGuaranteeIsNull();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForSubStrategy1WithGuarantee() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForSubStrategy1WithGuarantee();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForSubStrategy1WithGuarantee();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForSubStrategy2WithGuaranteeIsNull() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForSubStrategy2WithGuaranteeIsNull();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForSubStrategy2WithGuaranteeIsNull();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForSubStrategy2WithGuarantee() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForSubStrategy2WithGuarantee();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForSubStrategy2WithGuarantee();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForSubStrategy3WithGuaranteeIsNull() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForSubStrategy3WithGuaranteeIsNull();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForSubStrategy3WithGuaranteeIsNull();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
+    @Test
+    void calculateCreditForSubStrategy3WithGuarantee() {
+        EvaluationDto evaluationDto = EvaluatorServiceDataProvider.getEvaluationDtoForSubStrategy3WithGuarantee();
+        EvaluationResult evaluationResult = EvaluatorServiceDataProvider.getEvaluationResultForSubStrategy3WithGuarantee();
+
+        EvaluationResult result = evaluatorService.calculateCredit(evaluationDto);
+
+        assertEquals(evaluationResult, result);
+    }
+
 
     @Test
     void getResult() {
